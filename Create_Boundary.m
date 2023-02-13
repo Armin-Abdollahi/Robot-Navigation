72787b93d2989ff1925e9b6d488367a221d1cbb6
Armin Abdollahi <103449830+Armin-Abdollahi@users.noreply.github.com>
2023-02-13 20:02:48 +0330

    Delete Create_Boundary.m

--- Create_Boundary.m
+++
@@ -1,6 +0,0 @@
-function x = Create_Boundary(x,Lb,Ub)
-
-x = max(x,Lb);   % If the variable value is less than the lower boundary, it takes the value of the lower boundary with maximization
-x = min(x,Ub);   % If the variable value is greater than the upper boundary, it takes the value of the upper boundary with minimization
-
-end
