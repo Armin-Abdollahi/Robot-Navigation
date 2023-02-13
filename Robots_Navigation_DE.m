de837aa49cf787bd1b48564cd869dbf14d0b96c4
Armin Abdollahi <103449830+Armin-Abdollahi@users.noreply.github.com>
2023-02-13 22:28:57 +0330

    Add files via upload

--- Robots_Navigation_DE.m
+++ Robots_Navigation_DE.m
@@ -2,22 +2,20 @@ clc             % Clear command window
 clear           % Clear workspace
 close all       % Close all figure
 
-%=========================== Parameters Setting ===========================
-global NFE
-NFE = 0;                % Number Of Fitness Function Evaluations
-popsize = 30;           % Population Size
-Max_It = 100;           % Maximum Number Of Itererations
-Pc=0.5                  % Crossover Probability
-Lb = 0;                 % Lower Bound
-Ub = 10;                % Upper Bound
+%% Parameters setting
+popsize = 30;           % Population size
+Max_It = 100;           % Maximum number of itererations
+Pc = 0.5;               % Crossover probability
+Lower_Boundary = 0;     % Lower bound
+Upper_Boundary = 10;    % Upper bound
 
 for i=1:popsize
-    pop(i).x=unifrnd(lb,ub);
+    pop(i).x=unifrnd(Lower_Boundary,Upper_Boundary);
     pop(i)=fitness(pop(i),data); 
 end
 
 
-% Mutation
+%% Mutation
 
 
 beta=unifrnd(BetaLb,BetaUb);
@@ -26,43 +24,14 @@ u=pop(A).x+beta.*(pop(B).x-pop(C).x);
 
 
 
-
-% Crossover
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%=========================== Initialize agents ===========================
+%% Initialize agents
 total = 1;          % Number of robots
 iterations = 100;   % Number of iterations
 %x = [2 4 6 8];
 %y = [3 1 5 7];
 
 
-%======================= Initialize of environment ========================
+%% Initialize of environment
 page = zeros(500,500);       % Size of the environment
 
 x = 0:1:10;
@@ -72,55 +41,3 @@ plot(x,y,'linewidth',5);
 
 
 axis([0 10 0 10])
-%=========================================================================
-
-
-%============================== First Test ===============================
-
-
-
-
-
-
-
-%============================== Second Test ==============================
-
-
-
-
-
-
-
-%========================= Exists a valid agent? =========================
-
-
-
-
-
-
-
-%================= Move robot to position of best agent ==================
-
-
-
-
-
-
-
-%=========================== Reach the target? ===========================
-
-
-
-
-
-
-
-%============================= Update agents =============================
-
-
-
-
-
-
-
-%================================= Stop ==================================
