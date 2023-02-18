bf0ba05d0521f70144e4bce1bfdc80012fc6bc20
Armin Abdollahi <103449830+Armin-Abdollahi@users.noreply.github.com>
2023-02-18 22:31:26 +0330

    Add files via upload

--- Robots_Navigation_DE.m
+++ Robots_Navigation_DE.m
@@ -18,67 +18,44 @@ end
 %%
 for Iteration = 1:Max_It           % For each iteration
     
-    for i=1:Popsize                % For each Solution
+    for i=1:Popsize                % For each solution
         New_Pop = Pop(i);
         x = Pop(i).x;
         Q = [1:i-1 i+1:popsize];
         
-        S=randsample(Q,3);
+        S = randsample(Q,3);
         
-        A=S(1);
-        B=S(2);
-        C=S(3);
+        % Three other members of the population (A,B,C) are chosen at random, not including i itself.
+        A = S(1);
+        B = S(2);
+        C = S(3);
         
         % Mutation
-        
-        beta=unifrnd(BetaLb,BetaUb);
-        
-        u=pop(A).x+beta.*(pop(B).x-pop(C).x);
-        
+        Beta = unifrnd(Beta_Lb,Beta_Ub);                    % Beta is random number between lower and upper boundary
+        U = pop(A).x + Beta.*(pop(B).x - pop(C).x);
         
         % Crossover
+        j1 = randi([1 nvar]);
         
-        j1=randi([1 nvar]);
-        
-        j2=find(rand(1,nvar)<Pcross);
+        j2 = find(rand(1,nvar) < Pc);
         
         J=[j1 j2];
         
-        x(J)=u(J);
+        x(J) = U(J);
         
-        New_Pop.x=x;
-        New_Pop=fitness(New_Pop,data);
+        New_Pop.x = x;
+        New_Pop = fitness(New_Pop,data);
         
         % Update Pop And Global Pop
         
-        if New_Pop.fit<pop(i).fit
-            pop(i)=New_Pop;
+        if New_Pop.fit < pop(i).fit
+            pop(i) = New_Pop;
         end
         
-        if New_Pop.fit<gpop.fit
-            gpop=New_Pop;
+        if New_Pop.fit < gpop.fit
+            gpop = New_Pop;
         end
         
-        
     end
-    
-    
-    BEST(Iteration)=gpop.fit;
-    MEAN(Iteration)=mean([pop.fit]);
-    
-    NO=' Feasible';
-    if gpop.SCH>0
-        NO=' Infeasible';
-    end
-    
-    disp([ 'Iter = ' num2str(Iteration) ' BEST = ' num2str(BEST(Iteration)) NO ])
-    
-    
     
 end
-
-%% Mutation
-
-Beta = unifrnd(Lower_Boundary,Upper_Boundary);    % Beta is random number between lower and upper boundary
-        
-U = Pop(A).x + Beta.*(Pop(B).x - Pop(C).x);
