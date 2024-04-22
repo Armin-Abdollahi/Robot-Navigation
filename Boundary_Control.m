af4c498e0e13b2716f8a978558791c3d7beca8b0
Armin Abdollahi <Armin.Abdollahi.64@gmail.com>
2024-04-22 21:36:51 +0330

    Delete Boundary_Control.m

--- Boundary_Control.m
+++
@@ -1,6 +0,0 @@
-function x = Boundary_Control(x,Lower_Boundary,Upper_Boundary)
-
-x = max(x,Lower_Boundary);   % If the variable value is less than the lower boundary, it takes the value of the lower boundary with maximization
-x = min(x,Upper_Boundary);   % If the variable value is greater than the upper boundary, it takes the value of the upper boundary with minimization
-
-end
